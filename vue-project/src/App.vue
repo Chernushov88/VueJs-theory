7d3dc3316d2a2066a66904fb6f890a8f8da9c30c
Chernushov88 <Chernushov88@mail.ru>
2019-10-23 23:18:57 +0300

    Vue CLI&Webpack  vue-component

---
+++ vue-project/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>Parent: {{ carName }}</h1>
+    <app-counter></app-counter>
+    <app-car
+      :carName = "carName"
+      :carYear = "carYear"
+      :changeFunc = "changeNameToAudi"
+      @nameChenged = "carName = $event"
+      @counterUpdated = "counter = $event"
+    ></app-car>
+  </div>
+
+</template>
+
+<script>
+    import Car from './Car.vue'
+    import Counter from './Counter.vue'
+
+    export default {
+      data () {
+        return {
+          carName: 'Ford',
+          carYear: 2019
+        }
+      },
+      components: {
+          'appCar': Car,
+          'appCounter': Counter
+      },
+      methods: {
+        changeNameToAudi() {
+            this.carName = 'Audi'
+        }
+      }
+    }
+</script>
