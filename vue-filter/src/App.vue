926a5ff74715634d18db8dfb07086c85f73bbe40
Chernushov88 <Chernushov88@mail.ru>
2019-10-30 16:42:59 +0200

    vue-forms

---
+++ vue-filter/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h2>{{ title }}</h2>
+    <input type="text" v-model="searchName">
+
+    <ul>
+      <li v-for="name of filtererNames">{{name}}</li>
+    </ul>
+
+    <hr>
+
+    <app-list></app-list>
+  </div>
+</template>
+
+<script>
+    import ListMixin from './listMixin'
+
+export default {
+  data () {
+    return {
+      title: 'Hello I am Vue!'
+    }
+  },
+  mixins: [ListMixin],
+  filters: {
+    lowercase(value) {
+      return value.toLowerCase()
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-forms/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h2>Form inputs</h2>
+
+    <app-onoff v-model="switched"></app-onoff>
+
+    <div>
+      <h3 v-if="switched">Component is enable</h3>
+      <h3 v-else>Component is disable</h3>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  import Onoff from './Onoff.vue'
+
+    export default {
+        data () {
+            return {
+                switched: false
+            }
+        },
+        components: {
+            appOnoff: Onoff
+        }
+    }
+</script>
+
+
+
+
+<style scoped>
+
+</style>
+
